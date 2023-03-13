285f67a45afc79bb8501c42a00e294586dba3666
velimakinen <51906536+velimakinen@users.noreply.github.com>
2023-03-13 17:22:00 +0200

    Update br_index_nplcp.hpp
    Adding functions is_left_maximal(sample), is_right_maximal(sample)

--- src/br_index_nplcp.hpp
+++ src/br_index_nplcp.hpp
@@ -1216,6 +1216,34 @@ public:
         if (!reversed) return remap_inv[bwt[i]];
         return remap_inv[bwtR[i]];
     }
+    
+    /*
+     * check if rangeR contains more than one run
+     */
+    
+    bool is_right_maximal(br_sample_nplcp sample) 
+    {
+       uchar c = bwt_at(sample.rangeR.first,true);
+       br_sample_nplcp right = right_extension(c,sample);
+       if (sample.size() == right.size())
+          return 0;
+       else
+          return 1;
+    }
+
+    /*
+     * check if range contains more than one run
+     */
+    
+    bool is_left_maximal(br_sample_nplcp sample) 
+    {
+       uchar c = bwt_at(sample.rangeR.first,false);
+       br_sample_nplcp left = left_extension(c,sample);
+       if (sample.size() == left.size())
+          return 0;
+       else
+          return 1;
+    }
 
     /*
      * get number of runs in BWT
